11d3fbd79165c15fed78b71091843bb0814d37d6
kevinlogan94 <kevlogan7@gmail.com>
2021-03-29 22:44:01 -0400

    add husky, pretty-quick, buefy, prettier, scss configurations

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class NavBar extends Vue {}
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
+import Buefy from "buefy";
 
-createApp(App).mount("#app");
+Vue.use(Buefy);
+
+Vue.config.productionTip = true;
+
+new Vue({ render: (h) => h(App) }).$mount("#app");
