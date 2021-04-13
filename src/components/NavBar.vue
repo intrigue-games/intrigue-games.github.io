4d772c986b7765d23f2f4ee3b240b0d39d5c02ca
kevinlogan94 <kevlogan7@gmail.com>
2021-04-12 20:16:54 -0400

    adjust navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,29 @@
 <template>
-  <b-navbar :fixed-top="true" :transparent="true">
+  <b-navbar type="is-primary" :spaced="true" :fixed-top="true">
     <template #brand>
       <b-navbar-item class="brand" href="#">
-        <img style="max-height: 52px" src="@/assets/logo-crop.png" />
+        <b-navbar-item href="#"
+          ><img class="image" src="@/assets/logo-crop.png"
+        /></b-navbar-item>
+        <b-navbar-item href="https://twitter.com/intrigue_games">
+          <b-icon type="is-white" icon="twitter"></b-icon
+        ></b-navbar-item>
+        <b-navbar-item
+          href="https://www.youtube.com/channel/UCQPXkDppOggoio7FFq_dpcg"
+          ><b-icon type="is-white" icon="youtube"></b-icon
+        ></b-navbar-item>
       </b-navbar-item>
     </template>
-    <template #start>
-      <b-navbar-item href="#"> [twitter icon] </b-navbar-item>
-      <b-navbar-item href="#"> [YouTube icon] </b-navbar-item>
-    </template>
+    <template #start> </template>
     <template #end>
-      <b-navbar-item href="#story">Story</b-navbar-item>
-      <b-navbar-item href="#gameplay">Gameplay</b-navbar-item>
-      <b-navbar-item href="#music">Music</b-navbar-item>
-      <b-navbar-item href="#comingsoon">Coming Soon!</b-navbar-item>
+      <b-navbar-item class="has-text-white" href="#story">Story</b-navbar-item>
+      <b-navbar-item class="has-text-white" href="#gameplay"
+        >Gameplay</b-navbar-item
+      >
+      <b-navbar-item class="has-text-white" href="#music">Music</b-navbar-item>
+      <b-navbar-item class="has-text-white" href="#comingsoon"
+        >Coming Soon!</b-navbar-item
+      >
     </template>
   </b-navbar>
 </template>
@@ -26,9 +36,8 @@ import { Component } from "vue-property-decorator";
 export default class NavBar extends Vue {}
 </script>
 
-<style scoped>
-.brand {
-  /*margin-right: 1rem;*/
-  /*padding: 0;*/
+<style scoped lang="scss">
+.image {
+  max-height: 40px;
 }
 </style>
